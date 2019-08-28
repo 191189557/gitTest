1b3c4dfa12c7a5eb77a7ec530dfba4febe237a7e
191189557 <191189557@qq.com>
2019-08-28 15:50:36 +0800

    提交了一个git测试羡慕

---
+++ src/test/GitTset.java
@@ -0,0 +1,11 @@
+package test;
+
+public class GitTset {
+	public static void main(String[] args) {
+		GitTset gitTset=new GitTset();
+		gitTset.test();
+	}
+	public void test() {
+		System.out.println("GitTset");
+	}
+}	
